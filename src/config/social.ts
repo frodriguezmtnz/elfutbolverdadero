6ae78c941948858641c9924f95a2e084b4c3ea6a
frodriguezmtnz <f.rodriguezmtnz@gmail.com>
2026-08-29 23:53:38 +0200

    refactor: centralise social links in config file

---
+++ src/config/social.ts
@@ -0,0 +1,7 @@
+export const social = {
+  facebook: 'https://www.facebook.com/people/Xabier-Futbolverdadero/pfbid037HnTaT97viA5rfCpD38H24fSEbT4XRpM97oCD14zMZK6USyaNzY9pko7fp3pWG1gl/',
+  instagram: 'https://www.instagram.com/futbolverdadero_oficial/',
+  x: 'https://x.com/Futbolverdader2',
+  whatsapp: 'https://chat.whatsapp.com/EQyrPr8KFHXCXsyerqKSR7?mode=gi_t',
+  email: 'xabier.merino@elfutbolverdadero.com',
+} as const;
